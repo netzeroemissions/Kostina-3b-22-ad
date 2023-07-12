a167f0b9b136b5dec78f7bd20820c708c4c9ac28
nadezhda <1seligselig@gmail.com>
2023-07-13 01:15:48 +0300

    first commit

---
+++ Test-9.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = 0
+        self.width = 0
+
+    def square(self):
+        square = self.length * self.width
+        return square
